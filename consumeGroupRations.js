6ae2927127e316254f4486a54008d0a74b52713f
Ross-CH <ross.harvey71@gmail.com>
2020-05-04 01:29:39 +0100

    feat: reduce party's rations

--- consumeGroupRations.js
+++ consumeGroupRations.js
@@ -1,38 +1,59 @@
 const itemName = "Rations (1 day)";
+const itemNameSimple = "Ration" // This is purely for display purposes
 const amountConsumed = 1;
-const party = [
+const partyNames = [
     "Bumble Ratord",
     "Tip Dicard",
     "Bowe'ner"
 ];
-// If any players in the party are not found, do not reduce the party's rations.
+let party = [];
+// If any actors in the party are not found, do not reduce the party's rations.
 // This is to prevent the party rations being reduced unevenly if a member's name is misspelled
-let playerNotFound = false;
-let playerOutputList = "";
-let itemOutputList = "";
-let msg = "";
+let playerOrItemNotFound = false;
+let errorList = `<h3>Error:</h3><ul>`;
+let chatMsg = `<h2>Eat Rations x${amountConsumed}</h2><ul>`;
 
+console.clear();
 
-for (let playerName of party) {
-    // For each player in the party, check that their name matches up with the name of an actor
-    let player = game.actors.getName(playerName);
+// For each actor in the party, check that their name matches up with the name of an actor
+for (let actorName of partyNames) {
+    let actor = game.actors.getName(actorName);
 
-    if (!player) {
-        playerOutputList += "Not found:\t" + playerName + "\n";
-        playerNotFound = true;
+    // If an actor is found, add them to the list of confirmed a, otherwise produce an error and add it to the error list
+    if (actor) {
+        party.push(actor);
     } else {
-        let item = player.items.find(i => i.name === itemName);
-        playerOutputList += "Found: \t\t" + playerName + "\n";
-
-        // Check that each member of the party is carrying rations
-        if (!item) {
-            itemOutputList += playerName + " is not carrying " + itemName + ".\n";
-        } else {
-            itemOutputList += item.name + ": " + item.id + "\n";
-        }
-
+        errorList += `<li>Actor not found: ${actorName}</li>`
+        playerOrItemNotFound = true;
     }
 }
 
-msg = playerOutputList + "\n" + itemOutputList;
-console.log(msg);
+// Check each party member has the selected item recorded in their inventory
+for (let actor of party) {
+    let item = actor.items.find(i => i.name === itemName);
+    if (!item) {
+        errorList += `<li>${actor.name} is not carrying ${itemName}`
+        playerOrItemNotFound = true;
+    }
+}
+errorList += `</ul>`;
+
+if (!playerOrItemNotFound) {
+    for (let actor of party) {
+        let item = actor.items.find(i => i.name === itemName);
+
+        // If the actor has enough of the selected item,
+        // decrement the quantity of the actor's selected item by the chosen amount
+        if (item.data.data.quantity >= amountConsumed) {
+            item.data.data.quantity -= amountConsumed;
+            chatMsg += `<li>${actor.name}: ${item.data.data.quantity} left.</li>\n`
+        } else {
+            chatMsg += `<li>${actor.name}: Not enough ${itemNameSimple.length ? itemNameSimple : itemName}s. ${item.data.data.quantity} left.</li>`
+        }
+    }
+    chatMsg += `</ul>`;
+} else {
+    chatMsg += errorList;
+}
+
+ChatMessage.create({content: chatMsg})
